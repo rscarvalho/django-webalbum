b5f05000f1d6d6e499aacf4abb540ae68199f4e9
Rodolfo Carvalho <rodolfo.ueg@gmail.com>
2009-05-13 21:27:42 -0300

    Added a license, an initial README file, and a setup.py script

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from glob import glob
+from os.path import dirname, join
+
+MO_FILES = join(dirname(__file__), "LOCALE", "pt_BR")
+data_files = [
+    (MO_FILES,
+     join(MO_FILES, "django.po"),
+     join(MO_FILES, "django.mo")),
+]
+
+setup(name="django-webalbum",
+      version="0.1",
+      description="A simple picture album manager form django",
+      long_description="django-webalbum is a simple picture manager for django,"
+                       " which consists in organizing pictures into albums. \nIt "
+                       "is possibile to add a watermark, select a cover picture "
+                       "and an album highlight image.",
+      license="MIT",
+      platforms=["Any"],
+      author="Rodolfo da Silva Carvalho",
+      author_email="rodolfo@rcarvalho.eti.br",
+      url="http://github.com/rcarvalho/django-webalbum/",
+      packages=["webalbum"],
+      package_data={"webalbum": [
+            "locale/pt_BR/LC_MESSAGES/django.po",
+            "locale/pt_BR/LC_MESSAGES/django.mo"
+            ]},
+)
+
